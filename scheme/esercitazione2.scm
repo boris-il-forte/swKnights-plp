2017de74c0c27636b57ce8cb4daedca759f96976
Marcello Pogliani <pogliamarci@hotmail.it>
2013-03-22 22:50:31 +0100

    Variations on the vector-map function and count-elements

--- scheme/esercitazione2.scm
+++ scheme/esercitazione2.scm
@@ -7,6 +7,29 @@
         (cons (f (vector-ref v pos)) (loop (+ pos 1)))
         '())))
 
+; Vector map variants ...
+; (a) return a NEW vector instead of a list...
+(define (vector-map-new vect fun)
+  (let ((out (make-vector (vector-length vect))))
+    (let loop ((i 0))
+      (if (< i (vector-length vect))
+          (begin
+            (vector-set! out i (fun (vector-ref vect i)))
+            (loop (+ 1 i)))
+          out)))) 
+
+; (b) "map in place" 
+; note that (obiouvsly) the vector should be mutable :)
+(define (vector-map-in-place! vect fun)
+  (let loop ((i 0))
+    (if (< i (vector-length vect))
+        (begin
+          (vector-set! vect i (fun (vector-ref vect i)))
+          (loop (+ 1 i)))
+        vect)))
+
+; Construct a max-rows x max-cols matrix, initialized with
+; the "fill" element
 (define (make-matrix max-rows max-cols fill)
   (let ([vec (make-vector max-rows #f)])
     (let loop ([x 0])
@@ -35,6 +58,18 @@
               (+ res 1)
               (car stack)))))
 
+; A variant of count-nodes. Simpler than the one done
+; during the lecture: does anyone see any flaw in it?
+; (i tested the two procedures with some values and they seem
+; to give the same result)
+; Basically, it uses the stack frame instead of having an explicit stack
+; TODO I'm waiting for comments on this stuff =)
+(define (my-count-nodes l)
+  (cond
+    ([null? l] 0)
+    ([list? (car l)]
+          (+ (my-count-nodes (car l)) (my-count-nodes (cdr l))))
+    (else (+ 1 (my-count-nodes (cdr l))))))
 
 ;O insieme potenza
 
@@ -112,7 +147,8 @@
          a
          b)]))
 
-(define-syntax my-max2
+; evaluates once a and b, instead of evaluating two times the one that gets returned! Useful when the two expressions have side effects
+(define-syntax my-max2 
   (syntax-rules ()
     [(_ a b)
      (let ((new-a a)
