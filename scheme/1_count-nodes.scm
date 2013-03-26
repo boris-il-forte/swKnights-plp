46abbb8bc87a56bc7943e6667d98582aedd2a881
Emanuele Uliana <cloudstrife9999@tiscali.it>
2013-03-26 15:44:41 +0100

    aModifiche

--- scheme/1_count-nodes.scm
+++ scheme/1_count-nodes.scm
@@ -2,7 +2,8 @@
 
 ; Count nodes ricorsivo
 
-; Marcello Pogliani <marcello.pogliani AT mail.polimi.it>
+; Non considera le liste come alberi il cui primo elemento è la radice, ma come liste annidate "semplicemente"
+; ad esempio (count-nodes ((+ 1 2) 3 4)) => 5 e non 3
 
 (define (count-nodes l)
   (cond
@@ -11,10 +12,20 @@
           (+ (count-nodes (car l)) (count-nodes (cdr l))))
     (else (+ 1 (count-nodes (cdr l))))))
 
+; qui considera le liste come alberi (come nel testo dell'esercizio corretto)
+
+(define (count-nodes-tree l)
+  (cond 
+     ([null? l] 0)
+     ([list? l] (foldl + 1 (map count-nodes-tree (cdr l))))
+     (else 1)))
+
+(count-nodes-tree '(1 2 3))
+    
 ; Valori di test
 
-(count-nodes '('1 '2 '3)) ; ==> 6
-(count-nodes '(1 2 3 (+ 4 5))) ; ==> 6
-(count-nodes '(1 2 (+ 3 (- 2 3) (* 4 5 (prova prova prova))) 4)) ; ==> 14
-(count-nodes '((+ 1 2 3 4 5) '2 '3)) ; ==> 10
-(count-nodes '((+ 1 2 3 4 5) (+ 2 3 4) ((+ 1 2) 3))) ; ==> 14
+(count-nodes-tree '('1 '2 '3)) ; ==> 6
+(count-nodes-tree '(1 2 3 (+ 4 5))) ; ==> 6
+(count-nodes-tree '(1 2 (+ 3 ((+ 2 3 4 5) 2 3) (* 4 5 (prova prova prova))) 4)) ; ==> 14
+(count-nodes-tree '((+ 1 2 3 4 5) '2 '3)) ; ==> 10
+(count-nodes-tree '((+ 1 2 3 4 5) (+ 2 3 4) ((+ 1 2) 3))) ; ==> 14
