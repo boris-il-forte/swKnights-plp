e743721d3737bd34990ef1f784b1976671efcc09
Boris_il_forte <davide.tateo90@gmail.com>
2013-07-17 12:49:53 +0200

    Corretto bug
    - divisione incasinava i fattori...

--- prolog/simplifier.pl
+++ prolog/simplifier.pl
@@ -9,7 +9,7 @@ simplify(X,Y) :- factor(X, 1, R, P), !, algebricProd(P,R, Y).
 factor(X, N, 1, P) :- integer(X), P is X*N.
 factor(X, N, X, N) :- atomic(X).
 factor(X*Y, N, Z, R) :- factor(X, N, A1, P), factor(Y, P, A2, R), algebricProd(A1, A2, Z).
-factor(X/Y, N, Z, R) :- factor(Y, N, A2, Q), P is 1/Q, factor(X, P, A1, R), algebricDiv(A1, A2, Z). 
+factor(X/Y, N, Z, R) :- factor(Y, 1, A2, Q), P is N/Q, factor(X, P, A1, R), algebricDiv(A1, A2, Z). 
 
 %product
 algebricProd(0, _, 0) :- !.
