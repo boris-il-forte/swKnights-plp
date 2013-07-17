d078f441f858a27a43971a0f1d7f1b7e79b832ce
Boris_il_forte <davide.tateo90@gmail.com>
2013-07-17 17:22:42 +0200

    Lavoro vario prolog
    - passi avanti per semplificatore: gestione degli atomi (nidi di parentesi, funzioni predefinite). non funziona!
    - gradiente nel derivatore

--- prolog/derivator.pl
+++ prolog/derivator.pl
@@ -1,9 +1,15 @@
-%% Derivator frontend
+% Derivator
+% extended prolog version of Pradella's Prolog derivator 
+% Davide Tateo <boris.ilpossente@hotmail.it>
 
+% Derivator frontend
 derive(X, _, 0, Y) :- !, simplify(X, Y).
 derive(X, T, N, Y) :- integer(N), !, K is N-1, d(X, T, W), derive(W, T, K, Y).
 
-%% Internal derivation function
+gradient(_, [], []) :- !.
+gradient(X, [T|V], [DXDT|R]) :- !, derive(X, T, 1,DXDT), gradient(X, V, R).
+
+% Internal derivation function
 d(X+Y, T, DX+DY) :- !, d(X, T, DX), d(Y, T, DY).
 d(X-Y, T, DY-DX) :- !, d(X, T, DX), d(Y, T, DY).
 d(X*Y, T, DX*Y+X*DY) :- !, d(X, T, DX), d(Y, T, DY).
@@ -11,3 +17,19 @@ d(X^N, T, N*X^K*DX) :- !, integer(N), K is N-1, d(X, T, DX).
 d(-X, T, -DX) :- !,  d(X, T, DX).
 d(X, X, 1) :- !.
 d(C, _, 0) :- atomic(C), !.
+
+% built-in functions
+d(sin(T), T, cos(T)) :- !.
+d(cos(T), T, -cos(T)) :- !.
+d(tan(T), T, 1/(cos(T))^2) :- !.
+d(log(T), T, 1/T) :- !.
+d(exp(T), T, exp(T)) :- !.
+d(atan(T), T,  1/(1+T^2)) :- !.
+d(acos(T), T, -1/(1-T^2)^0.5) :- !.
+d(asin(T), T, 1/(1-T^2)^0.5) :- !.
+
+% Chain rule
+d(F_G, T, DF*DG) :- F_G =.. [_ , G], !, d(F_G, G, DF), d(G, T, DG).
+
+
+
--- prolog/simplifier.pl
+++ prolog/simplifier.pl
@@ -2,46 +2,71 @@
 % Inspired by Pradella's Prolog derivator
 % Davide Tateo <boris.ilpossente@hotmail.it>
 
-% simplify main routine
+%% simplify main routine
 simplify(_^0, 1) :- !.
 simplify(X^1, X) :- !.
-simplify(X,Y) :- term(X, 0, R, S), !, algebricSum(S, R, Y).
+simplify(X, Y) :- term(X, 0, R, S), !, algebricSum(S, R, Y).
 
-% simplify factors
-factor(X, N, 1, P) :- integer(X), !, P is X*N.
-factor(X, N, X, N) :- atomic(X), !.
-factor(X^Y, N, X^Y, N) :- atomic(X).
+%% simplify atoms
+
+% atoms and matematical functions
+atom(X, X) :- integer(X), !.
+atom(X, X) :- atomic(X), !.
+atom(sin(X), sin(X)) :- !.
+atom(cos(X), cos(X)) :- !.
+atom(tan(X), tan(X)) :- !.
+atom(exp(X), exp(X)) :- !.
+atom(log(X), log(X)) :- !.
+atom(atan(X), atan(X)) :- !.
+atom(asin(X), asin(X)) :- !.
+atom(acos(X), acos(X)) :- !.
+
+% exponentietion
+atom(_^0, 1) :- !.
+atom(X^1, X) :- !.
+atom(X^Y, Z) :- integer(X), integer(Y), !, Z is X^Y.
+atom(X^Y, X^Y) :- !.
+
+% nested parenthesis
+atom((X), Y) :- simplify(X, Y), atomic(Y), !.
+atom((X), (Y)) :- simplify(X, Y), !.
+
+%% simplify factors
+factor(X, N, 1, P) :- atom(X,Y), integer(Y), !, P is Y*N .
+factor(X, N, Y, N) :- atom(X,Y), !.
 factor(X*Y, N, Z, R) :- factor(X, N, A1, P), factor(Y, P, A2, R), !, algebricProd(A1, A2, Z).
 factor(X/Y, N, Z, R) :- factor(Y, 1, A2, Q), P is N/Q, factor(X, P, A1, R), !, algebricDiv(A1, A2, Z). 
 
-% simplify terms
+%% simplify terms
 term(X, N, Y, N) :- factor(X, 1, R, P), !, algebricProd(P, R, Y).
 term(X+Y, N, Z, S) :- term(X, N, A1, R), term(Y, R, A2, S), !, algebricSum(A1, A2, Z).
 term(X-Y, N, Z, S) :- term(X, N, A1, R1), term(Y, 0, A2, R2), !, algebricSub(A1, A2, Z), S is R1-R2.
 
 
+%% apply operations
+
 %product
 algebricProd(0, _, 0) :- !.
 algebricProd(_, 0, 0) :- !.
-algebricProd(1, A, Z) :- !, Z = A.
-algebricProd(A, 1, Z) :- !, Z = A.
-algebricProd(1.0, A, Z) :- !, Z = A.
-algebricProd(A, 1.0, Z) :- !, Z = A.
+algebricProd(1, A, Z) :- !, Z=A.
+algebricProd(A, 1, Z) :- !, Z=A.
+algebricProd(1.0, A, Z) :- !, Z=A.
+algebricProd(A, 1.0, Z) :- !, Z=A.
 algebricProd(A, A, A^2) :- !.
 algebricProd(A^N, A, A^K) :- integer(N), !, K is N+1.
 algebricProd(A, A^N, A^K) :- integer(N), !, K is N+1.
 algebricProd(A^M, A^N, A^K) :- integer(M), integer(N), !, K is M+N.
-algebricProd(A1, A2, Z) :- !, Z = A1*A2.
+algebricProd(A1, A2, Z) :- !, Z=A1*A2.
 
 % division
 algebricDiv(0, _, 0) :- !.
-algebricDiv(A, 1, Z) :- !, Z = A.
-algebricDiv(A, 1.0, Z) :- !, Z = A.
+algebricDiv(A, 1, Z) :- !, Z=A.
+algebricDiv(A, 1.0, Z) :- !, Z=A.
 algebricDiv(A, A, 1) :- !.
 algebricDiv(A^N, A, A^K) :- integer(N), !, K is N-1.
 algebricDiv(A, A^N, A^K) :- integer(N), !, K is N-1.
 algebricDiv(A^M, A^N, A^K) :- integer(M), integer(N), !, K is M-N.
-algebricDiv(A1, A2, Z) :- !, Z = A1/A2.
+algebricDiv(A1, A2, Z) :- !, Z=A1/A2.
 
 % sum
 algebricSum(0, X, X) :- !.
@@ -50,7 +75,7 @@ algebricSum(X, X, Y) :- !, algebricProd(2,X, Y).
 algebricSum(N*A, A,Y) :- integer(N), !, K is N+1, algebricProd(K, A, Y).
 algebricSum(A, N*A, Y) :- integer(N), !, K is N+1, algebricProd(K, A, Y).
 algebricSum(M*A, N*A, Y) :- integer(M), integer(N), !, K is M+N , algebricProd(K, A, Y).
-algebricSum(X, Y, Z) :- !, Z = X+Y.
+algebricSum(X, Y, Z) :- !, Z=X+Y.
 
 % subtraction
 algebricSub(0, X, -X) :- !.
@@ -59,6 +84,6 @@ algebricSub(X, X, 0) :- !.
 algebricSub(N*A, A,Y) :- integer(N), !, K is N-1, algebricProd(K, A, Y).
 algebricSub(A, N*A, Y) :- integer(N), !, K is 1-N, algebricProd(K, A, Y).
 algebricSub(M*A, N*A, Y) :- integer(M), integer(N), !, K is M-N , algebricProd(K, A, Y).
-algebricSub(X, Y, Z) :- !, Z = X-Y.
+algebricSub(X, Y, Z) :- !, Z=X-Y.
 
 
