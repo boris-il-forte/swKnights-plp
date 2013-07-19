82f87c49fcb33d5a45004e4ab5ca607156d95b0e
Boris_il_forte <davide.tateo90@gmail.com>
2013-07-19 20:54:50 +0200

    Lavoro su monadi:
    - creata monade MVector, un vettore dinamico monadico
    - non sono riuscito a fare il vettore monadico dinamico che fallisce...

---
+++ haskell/monadicVector.hs
@@ -0,0 +1,53 @@
+module MonadicVector where
+
+newtype MVector a b = MVector ((Int -> a) -> ((Int->a), b))
+
+instance Show b => Show(MVector a b) where
+	show (MVector x) = let (t,s) = x t in show s
+
+
+instance Monad (MVector vector) where
+	return x = let f t = (t,x)
+		   in MVector f
+	
+	MVector f >>= g = MVector(\oldvector -> 
+			   let (newvector, val) = f oldvector 
+			       MVector f'        = g val
+			   in f' newvector)
+
+-- initialize vector with a vector with policy described in the function f
+createVector f = MVector (\oldvector -> (emptyVector , ())) where
+	emptyVector n = f n
+	
+-- initialize vector with a vector of zeros
+createZeroVector = createVector (\n -> 0)
+
+-- initialize vector with a failing vector
+-- createFailingVector :: MVector a ()
+-- createFailingVector = createVector(\n -> let s ="the index" ++ (show n) ++ "is undefined" in s >>=  fail s)
+
+-- get a value from the vector
+getValue i 
+	| i >= 0  = MVector(\vector -> (vector, vector i))
+	| otherwise = fail "negative index"
+
+-- put a value in the vector 
+putValue (new, i)  
+	| i > 0 = MVector (\oldvector -> (newvector oldvector , ())) 
+	| otherwise = fail "negative index"
+	where
+		newvector oldvector n
+			| n == i = new
+			| otherwise = oldvector n
+		
+main = 	do
+	{
+		createZeroVector;
+		putValue(5,4);
+		x <- getValue(4);
+		y <- getValue(0);
+		putValue(x,6);
+		z <- getValue(6);
+		return (x+y+z);
+		
+	}
