ab929c7eb39d0591973e0dfb4b15f6d5bb9ba220
Boris_il_forte <davide.tateo90@gmail.com>
2013-07-21 21:31:50 +0200

    Casino su haskell... nulla di buono
    - casino...

--- haskell/PrologMonad.hs
+++ haskell/PrologMonad.hs
@@ -1,35 +1,34 @@
 module PrologMonad where
 
---  tentativo di implementare il backtracking del prolog in haskell...ovviamente non riuscito. Non rieco a implementare fail correttamente...
+--  tentativo di implementare il backtracking del prolog in haskell...ovviamente non riuscito.
 
-type ContAndWays r w a = ((a -> r), [w])
-newtype MProlog r w a = MProlog { runningContAndWays :: (ContAndWays r w a -> r) } -- r is the final result type of the whole computation 
- 
-instance Monad (MProlog r w) where 
-	return x = MProlog $ \k -> let (c, w) = k in  c x 
+-- todo: tipo sbagliato! fatto casino...
+type ContAndWays w a r = ((a -> r), [w])
+newtype MProlog w a r = MProlog { runningContAndWays :: (ContAndWays w a r -> (ContAndWays w a r , r)) } -- r is the final result type of the whole computation 
+
+instance Show r => Show (MProlog r w a) where
+	show (MProlog x) = let (cw, r) = x cw in show r
+
+instance Monad (MProlog _ w a) where 
+	return x = MProlog $ \k -> (k, x)
 	
 	(MProlog f) >>= g = MProlog $ \k -> 
 		let
 		{
 			(cont, ways) = k;
-			newcont = \a -> (runningContAndWays(g a)) k
+			newcont = \a -> let ((c, w), r) = (runningContAndWays(g a)) k in c a
 		}
 		in f (newcont, ways)
 		
-	fail s = do
-		{
-			w:ws <- getWays;
-			c <- getCont;
-			putContAndWays (c, ws);
-			callCont s c ws;
-		} where
-			callCont s c [] = s
-			callCont _ c w = (c w)
-	 
-getWays = MProlog(\k -> let (c,w) = k in (c, w, w))
-getCont = MProlog(\k -> let (c,w) = k in (c, w, c))    
-putContAndWays (c, w) = MProlog(\_ -> (c, w, ()))
-		
-		
+try (x:xs) = MProlog $ \k -> let (c, xs) = k in  c x
+try [] = fail "No options left"
+failC = MProlog $ \k -> let (c, x:xs) = k in  c x 
+			    
+main = do
+	{
+		x <- try [1,2,3];
+		if x == 3 then return x
+		else failC 
+	}
 
 
